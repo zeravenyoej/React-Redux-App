1337a1bc682d254b18a22b6301c240c04926336b
Joey Nevarez <josephnevarez12@gmail.com>
2020-01-10 14:31:01 -0800

    added empty reducer

--- practice/src/Reducer/reducer.js
+++ practice/src/Reducer/reducer.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { FETCH_DATA_START, FETCH_DATA_SUCCESS, FETCH_DATA_FAIL } from '../Actions/actions';
 
-const reducer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+const initialState = {
+
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case FETCH_DATA_START:
+            return {
+                ...state
+            };
+        case FETCH_DATA_SUCCESS:
+            return {
+                ...state
+            };
+        case FETCH_DATA_FAIL:
+            return {
+                ...state
+            };
+        default: 
+            return state;
+    }
 };
 
 export default reducer;
